c78a0808d660a013bcb169e7ea1e8e6fb3ab7b68
iEnyene <akpanenyene@gmail.com>
2022-09-12 20:25:06 +0100

    update settings for deployment

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(9-xiw^=k(pbodkhlp0#q6eybm9le0c_a1*&$-!wy=1+iwam=b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
