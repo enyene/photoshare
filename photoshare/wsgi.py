52d0d35c5acba737157a53e41211a8a3715ceca9
iEnyene <akpanenyene@gmail.com>
2022-09-12 20:23:02 +0100

    update wsgi

--- photoshare/wsgi.py
+++ photoshare/wsgi.py
@@ -1,10 +1,10 @@
 """
-WSGI config for photoshare project.
+WSGI config for bludrop project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
@@ -13,18 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photoshare.settings')
 
-application = get_wsgi_application()
-
-import os
-import sys
-
-# assuming your Django settings file is at
-# '/home/myusername/mysite/mysite/settings.py'
-path = '/home/enyene/photoshare'
-if path not in sys.path:
-     sys.path.insert(0, path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'photoshare.settings'
-
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
